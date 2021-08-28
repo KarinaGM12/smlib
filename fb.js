25b7a9f5e982d701d63f2953b11394393ed04d65
KarinaGM12 <karina@entropy.tech>
2021-08-28 14:42:51 -0500

    Create class DiscoveryFields for query fields param in user discovery

--- fb.js
+++ fb.js
@@ -1,6 +1,6 @@
 const helpers = require('./helpers/helpers')
+const discovery = require('./discovery')
 const https = require('https');
-
 /**
  * Get request to facebook API
  * @param {string} urlPath 
@@ -53,46 +53,30 @@ const Get = function (urlPath, params){
  * @returns {Promise} Promise object represents request payload
 */
 function DiscoverPosts(urlPath, fields, token,posts){
-    const params = {
-        'fields':builDiscoveryFields(fields.userName,fields.startDate,fields.endDate,fields.after),
-        'access_token':token
-    }
     
+    const reqFields = new discovery.DiscoveryFields(fields);
+    const params = {
+        'fields':reqFields.format(),
+        'access_token':token
+    }    
+
     return Get(urlPath,params).then((data)=>{
         if(data){
             if (data.business_discovery && data.business_discovery.media && data.business_discovery.media.data){
                 posts = [...posts, ...data.business_discovery.media.data];
                 const after = helpers.getAfterCursor(data.business_discovery.media)
                 if (after){
-                    fields.after = after
-                    return DiscoverPosts(urlPath,fields, token,posts)
+                    reqFields.setAfter(after)
+                    return DiscoverPosts(urlPath,reqFields, token,posts)
                 }
                 return posts
             }
         }
         return []
     }).catch((err)=>{
-        return err
+        throw (err)
     })
 }
-
-/**
- * This function build query 'fields' parameter for business_discovery request
- * @param {string} userName he userName of the instagram account to query (required)
- * @param {string} startDate Start date of request in ISO Layout 'YYYY-MM-DD'
- * @param {string} endDate End date of request in ISO Layout 'YYYY-MM-DD
- * @param {string} after Cursor value for following page in cursor based pagination
- * @returns {string} Request query parameter 'fields' as string
- */
-function builDiscoveryFields(userName,startDate,endDate,after){
-    let fields = ''
-    fields+=(helpers.isString(userName)?`business_discovery.username(${ userName }){media`:'');
-    fields+=(helpers.isString(startDate)?`.since(${startDate})`:`.since(${helpers.getStartDate()})`);
-    fields+=(helpers.isString(endDate)?`.until(${endDate})`:'');
-    fields+=(helpers.isString(after)?`.after(${after})`:'');
-    fields+=(fields.length > 0?'{timestamp,caption,media_type,children{media_url}}}':'');
-    return fields
-} 
 
 module.exports = {
     Get: Get,
