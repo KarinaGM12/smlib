c2548f50032f74539b1082342263130063592c8a
KarinaGM12 <karina@entropy.tech>
2021-08-30 23:18:14 -0500

    Add new test to validate error is thrown when input is not string

--- test/test-fields.js
+++ test/test-fields.js
@@ -9,10 +9,16 @@ describe('Fields',()=>{
 })
 
 describe('setItem',()=>{
-    it('adds item to set',()=>{
+    it('adds item to set if it is string',()=>{
         const obj = new fields.Fields();
         obj.setItem('test');
         assert.equal(obj.fields.has('test'),true);
+    })
+    it('throws error when attempting to set a non string item',()=>{
+        const obj = new fields.Fields();
+        assert.throws(()=>{
+            obj.setItem(1234)
+        },SyntaxError);
     })
 })
 
